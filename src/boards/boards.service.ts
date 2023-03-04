f7f170007ddecd842dd7355faf356c7434c7d6db
Saurabh S <saurabh.satralkar7@gmail.com>
2023-03-04 12:01:39 +0100

    Path/Update added

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -33,4 +33,10 @@ export class BoardsService {
         this.boards = this.boards.filter(board => board.id ! ==id);
     }
 
+    updateBoardStatus(id: string, status: BoardStatus): Board {
+        const board = this.getBoardById(id);
+        board.status = status
+        return board;
+    }
+
 }
